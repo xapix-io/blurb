2210e3250081b145de2d77f505060eb6c67c6921
Furkan Enes <feapaydin@gmail.com>
2023-06-30 12:25:39 +0300

    dynamic header support per request

--- lib/blurb/sp_requests.rb
+++ lib/blurb/sp_requests.rb
@@ -5,11 +5,12 @@ require 'blurb/request_collection'
 class Blurb
   # Adapter for ADS SP v3 report requests
   class SpRequests < RequestCollection
-    def post(endpoint, payload)
+    def post(endpoint, payload, headers)
       execute_request(
         api_path: endpoint,
         request_type: :post,
-        payload: payload
+        payload: payload,
+        headers: headers
       )
     end
   end
