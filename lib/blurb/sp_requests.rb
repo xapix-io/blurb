a6acc73d3fadc959acfa3b90f30bd3bc54aaa0b2
Furkan Enes <feapaydin@gmail.com>
2023-06-30 16:40:02 +0300

    compose request payload manually

--- lib/blurb/sp_requests.rb
+++ lib/blurb/sp_requests.rb
@@ -5,13 +5,19 @@ require 'blurb/request_collection'
 class Blurb
   # Adapter for ADS SP v3 report requests
   class SpRequests < RequestCollection
-    def targets_keywords_recommendations(payload)
+    def targets_keywords_recommendations(asins:, recommendation_type:, max_recommendations:, bids_enabled:, sort_dimension:)
       headers = @headers.merge('Content-Type' => 'application/vnd.spkeywordsrecommendation.v3+json')
       execute_request(
         api_path: '/targets/keywords/recommendations',
         request_type: :post,
         headers: headers,
-        payload: payload
+        payload: {
+          asins: asins,
+          recommendationType: recommendation_type,
+          maxRecommendations: max_recommendations,
+          bidsEnabled: bids_enabled,
+          sortDimension: sort_dimension
+        }
       )
     end
   end
