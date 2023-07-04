73b26af40c637af8de99c4c30473f98c6686c88f
Furkan Enes <feapaydin@gmail.com>
2023-07-04 19:11:34 +0300

    removed marketplace id

--- lib/blurb/sp_requests.rb
+++ lib/blurb/sp_requests.rb
@@ -5,10 +5,9 @@ require 'blurb/request_collection'
 class Blurb
   # Adapter for ADS SP v3 report requests
   class SpRequests < RequestCollection
-    def targets_keywords_recommendations(asins:, recommendation_type:, max_recommendations:, bids_enabled:, sort_dimension:, marketplace_id:)
+    def targets_keywords_recommendations(asins:, recommendation_type:, max_recommendations:, bids_enabled:, sort_dimension:)
       headers = @headers.merge(
         'Content-Type' => 'application/vnd.spkeywordsrecommendation.v3+json',
-        'Amazon-Advertising-API-MarketplaceId' => marketplace_id
       )
       execute_request(
         api_path: '/targets/keywords/recommendations',
