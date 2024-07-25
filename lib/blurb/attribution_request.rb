0f3efa1575ed3944ed6fa50cfb169a4f2db4cdc5
Furkan Enes <feapaydin@gmail.com>
2024-07-25 18:40:37 +0300

    removed defaults from requester params

--- lib/blurb/attribution_request.rb
+++ lib/blurb/attribution_request.rb
@@ -10,25 +10,8 @@ class Blurb
       @base_url = "#{base_url}/report"
     end
 
-    PERFORMANCE_REPORT_METRICS = %w[
-      Click-throughs
-      attributedDetailPageViewsClicks14d
-      attributedAddToCartClicks14d
-      attributedPurchases14d
-      unitsSold14d
-      attributedSales14d
-      attributedTotalDetailPageViewsClicks14d
-      attributedTotalAddToCartClicks14d
-      attributedTotalPurchases14d
-      totalUnitsSold14d
-      totalAttributedSales14d
-      brb_bonus_amount
-      kindleEditionNormalizedPagesRead14d
-      kindleEditionNormalizedPagesRoyalties14d
-    ].freeze
-
     # https://advertising.amazon.com/API/docs/en-us/amazon-attribution-prod-3p#tag/Reports/operation/getAttributionTagsByCampaign
-    def report(start_date:, end_date:, group_by:, report_type: 'PERFORMANCE', metrics: nil, advertiser_ids: nil, cursor_id: nil, count: 5000) # rubocop:disable Metrics/ParameterLists, Metrics/MethodLength
+    def report(start_date:, end_date:, report_type:, group_by:, metrics: nil, advertiser_ids: nil, cursor_id: nil, count: 5000) # rubocop:disable Metrics/ParameterLists, Metrics/MethodLength
       execute_request(
         request_type: :post,
         payload: {
@@ -38,7 +21,7 @@ class Blurb
           groupBy: group_by,
           cursorId: cursor_id,
           advertiserIds: advertiser_ids,
-          metrics: metrics || PERFORMANCE_REPORT_METRICS.join(','),
+          metrics: metrics,
           count: count
         }.compact
       )
